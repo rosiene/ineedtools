8f9078f8d7e0ca3593c736b03ec14104239b8623
Rosiene <rosiene.dilly@gmail.com>
2016-06-17 11:53:33 +0200

    upload image and create new column imagea and delete column link in picture table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160616133153) do
+ActiveRecord::Schema.define(version: 20160617093759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,9 +38,9 @@ ActiveRecord::Schema.define(version: 20160616133153) do
 
   create_table "pictures", force: :cascade do |t|
     t.integer  "offer_id"
-    t.string   "link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
   add_index "pictures", ["offer_id"], name: "index_pictures_on_offer_id", using: :btree
